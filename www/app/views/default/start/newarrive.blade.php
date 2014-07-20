923dbf4e4e650cc519189e2a47b0e48067f8a58e
iamcot <thang102@gmail.com>
2014-07-20 21:53:25 +0700

    fix start page

--- www/app/views/default/start/newarrive.blade.php
+++ www/app/views/default/start/newarrive.blade.php
@@ -2,14 +2,49 @@
     <h1 class="text-center">
         Latest Arrivals
     </h1>
+
     <p class="text-center" style=" color:#676767;">This time we have something very hot for you!</p>
     <br>
-    <ul class="grid">
-    {{--*/ $lists = Vproduct::where('isnews','0')->orderby('laview','desc')->orderby('id','desc')->take(12)->get(); /*--}}
+    {{--*/ $lists = Vproduct::where('isnews','0')->orderby('laview','desc')->orderby('id','desc')->take(24)->get();
+    /*--}}
     {{--*/ $noInfo = true; /*--}}
-    @foreach($lists as $list)
-    @include(Config::get('shop.theme').'/list/startlistitem')
-    @endforeach
-    </ul>
-    <div class="clearfix"></div>
+    {{--*/ $i = 0;$countnews = count($lists); /*--}}
+    @if($countnews >0)
+    <div id="carousel-example-genericnews" class="carouselnews " data-ride="carousel">
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+            @foreach($lists as $list)
+            @if($i%12==0)
+            <div class="item @if($i==0) active @endif">
+                <ul class="grid">
+            @endif
+                        @include(Config::get('shop.theme').'/list/startlistitem')
+                        {{--*/ $i +=1 /*--}}
+                        @if($i%12==0 && $i< $countnews)
+                    </ul>
+            </div>
+            @endif
+            @endforeach
+            </ul>
+        </div>
+        <div class="newarrivercontrol">
+            <a class="left carousel-control" href="#carousel-example-genericnews" data-slide="prev">
+            </a>
+            <a class="right carousel-control" href="#carousel-example-genericnews" data-slide="next">
+            </a>
+        </div>
+    </div>
 </div>
+@endif
+
+<div class="clearfix"></div>
+</div>
+@section('jscript')
+@parent
+<script>
+    $('.carouselnews').carousel({
+        animation: 'slow'
+    });
+</script>
+
+@stop
