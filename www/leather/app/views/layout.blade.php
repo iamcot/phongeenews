68803464faa9859266271e6149a5e5f4f2fc0a05
thang.truong <thang.truong@lazada.com>
2014-11-22 19:05:30 +0700

    update zopim

--- www/leather/app/views/layout.blade.php
+++ www/leather/app/views/layout.blade.php
@@ -132,14 +132,13 @@ $( window ).scroll(function() {
   ga('send', 'pageview');
 
 </script>
-
 <!--Start of Zopim Live Chat Script-->
 <script type="text/javascript">
 window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
-                                                                      d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
-    _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
-    $.src='//v2.zopim.com/?2PGxsPoFfu688QVx12CIo2r8pISJeODW';z.t=+new Date;$.
-                                                                               type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
+d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+_.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
+$.src='//v2.zopim.com/?2agbYPCLvXEEzKVhf5UQEb5K9RSWpptg';z.t=+new Date;$.
+type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
 </script>
 <!--End of Zopim Live Chat Script-->
 @endif
